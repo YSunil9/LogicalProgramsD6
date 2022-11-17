55118b9b3ec416f37ccf3c95fd8e33a07fbbde19
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-17 22:41:43 +0530

    Code

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = 0, val2 = 1, addition = 0;
+
+            Console.WriteLine("Enter value  ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("0 \n1");
+            for (int i = 2; i < length; i++)
+            {
+                addition = val1 + val2;
+                Console.WriteLine(addition);
+                val1 = val2;
+                val2 = addition;
+            }
+        }
+    }
+}
