48b231370cd37c445b971d367c80173a66437079
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-20 21:51:14 +0530

    perfect number code

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -4,18 +4,27 @@
     {
         static void Main(string[] args)
         {
-            int val1 = 0, val2 = 1, addition = 0;
-
-            Console.WriteLine("Enter value  ");
-            int length = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("0 \n1");
-            for (int i = 2; i < length; i++)
+            Console.WriteLine("Enter the number to be checked:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0, num;
+            num = n;
+            for (int i = 1; i < n; i++)
             {
-                addition = val1 + val2;
-                Console.WriteLine(addition);
-                val1 = val2;
-                val2 = addition;
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                }
+
             }
+            if (sum == num)
+            {
+                Console.WriteLine("The number is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine("The number is not a perfect number");
+            }
+            Console.ReadLine();
         }
     }
 }
