693cc04373de1a7ab5bba82c4af4d76a0920d0ce
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-20 22:16:53 +0530

    Reverse number code

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,25 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the number to be checked:");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            for (int i = 1; i <= n; i++)
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;
+            int remainder = 0;
+            while (number > 0)
             {
-                if (n % i == 0)
-                {
-                    sum++;
-                }
-
+                remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number = number / 10;
             }
-            if (sum == 2)
-            {
-                Console.WriteLine("The number is a prime number");
-            }
-            else
-            {
-                Console.WriteLine("The number is not a prime number");
-            }
-            Console.ReadLine();
+            Console.WriteLine("Reversed Number is :" + reverse);
         }
     }
 }
