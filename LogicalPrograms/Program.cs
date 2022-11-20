065260f01df7b8150c4a6d5f7e55c973de5e5cc6
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-20 22:04:32 +0530

    Prime number code

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,23 +6,22 @@
         {
             Console.WriteLine("Enter the number to be checked:");
             int n = Convert.ToInt32(Console.ReadLine());
-            int sum = 0, num;
-            num = n;
-            for (int i = 1; i < n; i++)
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
             {
                 if (n % i == 0)
                 {
-                    sum = sum + i;
+                    sum++;
                 }
 
             }
-            if (sum == num)
+            if (sum == 2)
             {
-                Console.WriteLine("The number is a perfect number");
+                Console.WriteLine("The number is a prime number");
             }
             else
             {
-                Console.WriteLine("The number is not a perfect number");
+                Console.WriteLine("The number is not a prime number");
             }
             Console.ReadLine();
         }
